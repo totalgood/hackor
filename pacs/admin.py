9afc30a7ec3e58f94efa4f67c9ae3dc0f09345b3
Hobson Lane <hobsonlane@gmail.com>
2015-09-13 16:16:13 -0700

    register all models in admin.py

--- pacs/admin.py
+++ pacs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.db.models import get_app, get_models
 
-# Register your models here.
+# register all the models this app:
+app = get_app('pacs')
+for model in get_models(app):
+    admin.site.register(model)
