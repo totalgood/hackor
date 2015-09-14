81a6552eebc99e6638c3f65af9b01e5db8e8cd5e
Hobson Lane <hobsonlane@gmail.com>
2015-09-13 17:24:37 -0700

    csv instead of semi-csv

---
+++ data/save_tables_to_csv.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION db_to_csv(path TEXT) RETURNS void AS $$
+declare
+  tables RECORD;
+  statement TEXT;
+begin
+  FOR tables IN
+    SELECT (table_schema || '.' || table_name) AS schema_table
+    FROM information_schema.tables t INNER JOIN information_schema.schemata s
+    ON s.schema_name = t.table_schema
+    WHERE t.table_schema NOT IN ('pg_catalog', 'information_schema', 'configuration')
+    ORDER BY schema_table
+  LOOP
+    statement := 'COPY ' || tables.schema_table || ' TO ''' || path || '/' || tables.schema_table || '.csv' ||''' DELIMITER '','' CSV HEADER';
+    EXECUTE statement;
+  END LOOP;
+  return;
+end;
+$$ LANGUAGE plpgsql;
+
+SELECT db_to_csv('/tmp');
