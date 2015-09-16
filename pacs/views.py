52045a5c9a231d92463efc2586e1737fa6ce8f2a
Thunder <thunder@uoregon.edu>
2015-09-15 20:59:22 -0700

    Updated serializers and views for API

--- pacs/views.py
+++ pacs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from hackor.pacs.models import RawCommitteeTransactions
+from rest_framework import viewsets
+from hackor.pacs.serializers import RawCommitteeTransactionsSerializer
 
-# Create your views here.
+class RawCommitteeTransactionsViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows Raw Committee Transactions to be viewed."""
+    queryset = RawCommitteeTransactions.objects.all
