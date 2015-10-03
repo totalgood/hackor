182635650990ab1c47182d77fdcde664dd110f47
Thunder <thunder@uoregon.edu>
2015-10-03 16:49:53 -0700

    Extends serializer to include all fields.

--- pacs/serializers.py
+++ pacs/serializers.py
@@ -7,5 +7,4 @@ from rest_framework import serializers
 class RawCommitteeTransactionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = RawCommitteeTransactions
-        fields = ('tran_id', 'tran_date', 'amount', 'aggregate_amount') 
         
