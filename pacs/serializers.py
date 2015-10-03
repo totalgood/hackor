a8ae8d01fb64d11605f2a7becbce9574f98e369b
Thunder <thunder@uoregon.edu>
2015-10-03 15:44:23 -0700

    Finished initial working REST api

--- pacs/serializers.py
+++ pacs/serializers.py
@@ -4,8 +4,8 @@ from pacs.models import RawCommitteeTransactions
 from rest_framework import serializers
 
 
-class RawCommitteeTransactionsSerializer(serializers.HyperlinkedModelSerializer):
+class RawCommitteeTransactionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = RawCommitteeTransactions
-        fields = ('tran_id', )
+        fields = ('tran_id', 'tran_date', 'amount', 'aggregate_amount') 
         
