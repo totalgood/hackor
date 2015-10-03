7a1cf8e91e1847b8f15c50d5803a697aa666e61e
Thunder <thunder@uoregon.edu>
2015-10-03 16:41:31 -0700

    Refactored API to use ViewSets. Added API website interface.

--- hackor/settings.py
+++ hackor/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'rest_framework',
     'django_extensions',
     'pacs',
 )
@@ -87,3 +87,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+        'PAGE_SIZE': 10
+
+}
