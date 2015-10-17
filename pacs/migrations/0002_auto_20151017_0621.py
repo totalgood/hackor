496e10a88162b6e14e73a2d189bae422daae29cd
Allen Grimm <agrimm@mobilepaks.com>
2015-10-16 23:23:06 -0700

    Adding migration previous changes to RawCommitteeTransactions model.

---
+++ pacs/migrations/0002_auto_20151017_0621.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pacs', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='rawcommitteetransactions',
+            options={},
+        ),
+    ]
