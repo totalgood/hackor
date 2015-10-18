313897f7cb659343e154e1171f23a3e307659d04
Hobson Lane <hobsonlane@gmail.com>
2015-10-17 17:41:27 -0700

    api works for most tables (without Decimal field values (large ints)) but django-filters don't seem to do anything

--- hackor/urls.py
+++ hackor/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.conf.urls import url
+from django.apps import apps
+from django.conf import settings
 from django.contrib import admin
-from pacs.views import CommitteeTransactionsViewSet, RawCommitteeTransactionsViewSet
+# from pacs.views import CommitteeTransactionsViewSet, RawCommitteeTransactionsViewSet
 from rest_framework.routers import DefaultRouter
 
+from pacs import views
 admin.autodiscover()
 
 '''urlpatterns = patterns('',
@@ -18,13 +22,19 @@ admin.autodiscover()
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'pacs', RawCommitteeTransactionsViewSet)
-router.register(r'pac_transactions', CommitteeTransactionsViewSet)
+# router.register(r'pacs', RawCommitteeTransactionsViewSet)
+
+
+for app_name in settings.APPS_TO_REST:
+    app = apps.get_app_config(app_name)
+    for lowercase_model_name, Model in app.models.iteritems():
+        model_name = Model._meta.object_name
+        viewset_class_name = model_name + 'ViewSet'
+        router.register(app_name + '/' + model_name, getattr(views, viewset_class_name))
+
 
 # The API URLs are now determined automatically by the router.
-
 urlpatterns = [
-	url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
 ]
-
