46067c4c3c73a1e03ba49539ab8847669478584c
Hobson Lane <hobsonlane@gmail.com>
2015-10-24 10:14:40 -0700

    generalize filed_date in model_queries for tranactions

--- pacs/model_queries.py
+++ pacs/model_queries.py
@@ -1,8 +1,9 @@
+from model_utils import models
 from pacs.models import RawCommitteeTransactions
 from django.db.models import Sum
 
 
-def top_filer_ids(sign=+1, n=500, start_date=None, end_date=None, table=RawCommitteeTransactions):
+def top_filer_ids(sign=+1, n=500, start_date=None, end_date=None, date_field=None, table=RawCommitteeTransactions):
     """Compile sorted list of committees (filter IDs) by tranaction ammount
 
     Args:
@@ -11,13 +12,19 @@ def top_filer_ids(sign=+1, n=500, start_date=None, end_date=None, table=RawCommi
       n (int): Maximum number of filer_ids to return
       table (Model): model to query (must have 'amount', 'filer_id', and 'tran_date' fields)
     """
+    date_field = date_field or (f for f in table.fields
+                                if isinstance(f, (models.DateTimeField, models.DateField))).next()
+    if not isinstance(date_field, basestring):
+        date_field = date_field.name
     qs = table.objects
     if sign > 0:
         qs = qs.filter(amount__gt=0)
     if sign < 0:
         qs = qs.filter(amount__lt=0)
     if start_date:
-        qs = qs.filter(trans_date__gte=start_date)
+        kwargs = {date_field + '__gte': start_date}
+        qs = qs.filter(**kwargs)
     if end_date:
-        qs = qs.filter(trans_date__lte=end_date)
+        kwargs = {date_field + '__lte': end_date}
+        qs = qs.filter(**kwargs)
     return qs.values('filer_id').annotate(total=Sum('amount')).order_by('-total').values()[:n]
