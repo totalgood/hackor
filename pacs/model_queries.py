115aebef7471b02ca46ab88b953dbf82cff76aac
Hobson Lane <hobsonlane@gmail.com>
2015-10-24 09:22:40 -0700

    disable all migrations

--- pacs/model_queries.py
+++ pacs/model_queries.py
@@ -2,18 +2,22 @@ from models import RawCommitteeTransactions
 from django.db.models import Sum
 
 
-def top_filer_ids(sign=+1, n=500, table=RawCommitteeTransactions):
+def top_filer_ids(sign=+1, n=500, start_date=None, end_date=None, table=RawCommitteeTransactions):
     """Compile sorted list of committees (filter IDs) by tranaction ammount
 
     Args:
       sign (int):  Determines subset of transactions summed
         positive = +1, negative = -1, or net = 0
       n (int): Maximum number of filer_ids to return
-      table (Model): model to query (must have 'amount' and 'filer_id' fields)
+      table (Model): model to query (must have 'amount', 'filer_id', and 'tran_date' fields)
     """
     qs = table.objects
     if sign > 0:
         qs = qs.filter(amount__gt=0)
     if sign < 0:
         qs = qs.filter(amount__lt=0)
+    if start_date:
+        qs = qs.filter(trans_date__gte=start_date)
+    if end_date:
+        qs = qs.filter(trans_date__lte=end_date)
     return qs.values('filer_id').annotate(total=Sum('amount')).order_by('-total').values()[:n]
