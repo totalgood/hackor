ec23d6300b5bc1b419d3bd55ead939a2523594a6
Hobson Lane <hobsonlane@gmail.com>
2015-10-24 12:42:58 -0700

    attempt to reset migrations without DROP TABLE django_migrations

--- pacs/models.py
+++ pacs/models.py
@@ -6,5 +6,5 @@ host = settings.DATABASES['default']['HOST'].lower()
 
 if '.rds.' in host or host.endswith('amazonaws.com'):
     from models_rds import *
-elif localhost in host or '127.0.0.1' in host or '192.168.' in host:
+elif 'localhost' in host or '127.0.0.1' in host or '192.168.' in host:
     from models_local import *
