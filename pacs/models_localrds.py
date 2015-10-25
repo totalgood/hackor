1859a5ba9ddfa1ff0450414b1288e1367f20ff42
Hobson Lane <hobsonlane@gmail.com>
2015-10-25 10:41:48 -0700

    revert models_localrds to nonpk id field since that is whats in rds

--- pacs/models_localrds.py
+++ pacs/models_localrds.py
@@ -585,7 +585,7 @@ class RawCommittees(models.Model):
 
 class RawCommitteesScraped(models.Model):
     name = models.TextField(blank=True, null=True)
-    id = models.IntegerField(primary_key=True, blank=True, null=False)
+    committee_id = models.IntegerField(blank=True, null=True)
     acronym = models.TextField(blank=True, null=True)
     pac_type = models.TextField(blank=True, null=True)
     filing_effective_from = models.TextField(blank=True, null=True)
