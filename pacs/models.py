9f6667c626ff8db20e7d3b19c1447fd4baa0bcd2
Hobson Lane <hobsonlane@gmail.com>
2015-10-25 10:14:18 -0700

    models file version grow like weeds

--- pacs/models.py
+++ pacs/models.py
@@ -3,8 +3,12 @@ from utils import models
 from utils import LongCharField
 
 host = settings.DATABASES['default']['HOST'].lower()
+dbname = settings.DATABASES['default']['NAME'].lower()
 
-if '.rds.' in host or host.endswith('amazonaws.com'):
+# use a recent localhost mirror of the RDS db created by Daniel
+if host in ('localhost', '127.0.0.1', '192.168.0.0') and dbname == 'btc':
+    from models_localrds import *
+elif '.rds.' in host or host.endswith('amazonaws.com'):
     from models_rds import *
 elif 'localhost' in host or '127.0.0.1' in host or '192.168.' in host:
     from models_local import *
