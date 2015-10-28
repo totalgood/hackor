26acc72f1df0cb3383f91ba0b8e281b8c69eaaa6
Hobson Lane <hobsonlane@gmail.com>
2015-10-27 17:06:16 -0700

    update ipython notebooks to reflect talk slides

--- bin/email_intersection_more_tlds.py
+++ bin/email_intersection_more_tlds.py
@@ -14,8 +14,8 @@ emails = ''
 mem_MB = 2000
 
 
-def extract_emails(top_tlds=None):
-    top_tlds = {
+def extract_emails(top_tlds=None, colnum=1):
+    top_tlds = top_tlds or {
             '.com': ('Commercial', 4860000000),
             '.org': ('Noncomercial', 1950000000),
             '.edu': ('US accredited postsecondary institutions', 1550000000),
@@ -39,23 +39,28 @@ def extract_emails(top_tlds=None):
             }
     tlds = set(top_tlds)
     email_regex = re.compile('[a-zA-Z0-9-.!#$%&*+-/=?^_`{|}~]+@[a-zA-Z0-9-.]+(' + '|'.join(tlds) + ')')
+    emails = ''
     with open('/home/hobs/Downloads/dmps/aminno_member_email.csv') as f:
         reader = csv.reader(f)
         for i, row in enumerate(reader):
-            em = email_regex.search(em)
+            em = email_regex.search(row[colnum])
             if em:
-                emails += em.group() + '\n'
+                emails += em.group().replace(',', '\n') + '\n'
             if len(emails) > mem_MB * 1000000:
                 break
             if not (i % 100000):
-                print(i / 100000.)
+                print("{}M emails read".format(i / 1000000.))
+    return emails
+
+
+emails = extract_emails()
 with open('/home/hobs/Downloads/dmps/emails3gb.txt', 'w') as f:
     f.write(emails)
-print(emails[:100])
+# print(emails[:100])
 del emails
 emails = pd.DataFrame.from_csv('/home/hobs/Downloads/dmps/emails3gb.txt').index
 intersection = []
-candidates = pd.DataFrame.from_csv('public.raw_candidate_filings.csv')  # id_nmbr
+candidates = pd.DataFrame.from_csv('../data/public.raw_candidate_filings.csv')  # id_nmbr
 candidates = set(candidates['email'].unique())
 for i, em in enumerate(candidates):
     if str(em).lower().strip() in emails:
